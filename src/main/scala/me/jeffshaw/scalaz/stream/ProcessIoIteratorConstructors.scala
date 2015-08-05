2e77aaf21e97e5e1e60fc794438f06db295d7d9a
Jeff Shaw <jshaw@rocketfuel.com>
2015-08-05 13:13:34 -0400

    Add iterators constructor.

--- src/main/scala/me/jeffshaw/scalaz/stream/ProcessIoIteratorConstructors.scala
+++ src/main/scala/me/jeffshaw/scalaz/stream/ProcessIoIteratorConstructors.scala
@@ -7,6 +7,8 @@ object ProcessIoIteratorConstructors {
   /**
    * Create a Process from an iterator that requires some external or
    * other mutable resource, while ensuring that the resource is released.
+   *
+   * Use `iterators` if the resource is associated with multiple iterators.
    */
   def iterator[R, O](
     acquire: Task[R]
@@ -14,5 +16,21 @@ object ProcessIoIteratorConstructors {
   )(release: R => Task[Unit]
   ): Process[Task, O] = {
     Process.await(acquire)(r => Process.eval(createIterator(r)).flatMap(IteratorConstructors.iteratorGo).onComplete(Process.eval_(release(r))))
+  }
+
+
+  /**
+   * Create a Process from an external resource associated with multiple
+   * iterators, while ensuring that the resource is released.
+   *
+   * Use `merge.mergeN` on the result to interleave the iterators, or
+   * .flatMap(identity) to emit them in order.
+   */
+  def iterators[R, O](
+    acquire: Task[R]
+  )(createIterators: R => Process[Task, Iterator[O]]
+  )(release: R => Task[Unit]
+  ): Process[Task, Process[Task, O]] = {
+    Process.await(acquire)(r => createIterators(r).map(IteratorConstructors.iteratorGo).onComplete(Process.eval_(release(r))))
   }
 }
